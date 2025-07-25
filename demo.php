52b6c41769225ebbf16ebcab049c47cc0d5cb5d1
iKabir7 <ikh2998@gmail.com>
2025-07-26 01:25:58 +0600

    new file created

---
+++ demo.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Simple HTML Page</title>
+</head>
+<body>
+
+    <h1>Welcome to My Website</h1>
+
+    <p>This is a simple HTML example. You can add text, images, links, and more!</p>
+
+    <form action="submit.php" method="POST">
+        <label for="name">Your Name:</label><br>
+        <input type="text" id="name" name="name" required><br><br>
+
+        <label for="email">Your Email:</label><br>
+        <input type="email" id="email" name="email" required><br><br>
+
+        <input type="submit" value="Submit">
+    </form>
+
+</body>
+</html>
